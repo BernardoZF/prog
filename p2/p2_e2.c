de131194dba0d68aef54e32db304980ebe257a8d
alvarxp <47323193+alvarxp@users.noreply.github.com>
2020-03-15 14:12:16 +0100

    Ej2 nice

--- p2/p2_e2.c
+++ p2/p2_e2.c
@@ -13,7 +13,7 @@
 *
 * @param in Pointer to the infix expression
 * @param suf Pointer to the suffix expression
-6
+*
 * @return The function returns OK on success or ERROR
 * when any of its parametrs is NULL
 **/
@@ -23,14 +23,8 @@ Status infix2postfix (char *suf, const char *in);
 
 Bool isOperator (char c);
 
-int main(){
-
-
-    return 0;
-}
-
 Bool isOperator (char c){
-  char array[] = OPERATORS; 
+  char array[] = OPERATORS;
   int i;
 
   for(i=0;i<6;i++){
@@ -42,14 +36,14 @@ Bool isOperator (char c){
   return FALSE;
 }
 
-int prec (char c){
+int prec (char *c){
   char array[] = OPERATORS;
 
-  if(array[0] == c || array[1] == c) return 0;
+  if(array[0] == *c || array[1] == *c) return 0;
 
-  if(array[2] == c || array[3] == c || array[4] == c) return 1;
+  if(array[2] == *c || array[3] == *c || array[4] == *c) return 1;
 
-  if(array[5] == c) return 2;
+  if(array[5] == *c) return 2;
 
   return -1;
 }
@@ -67,63 +61,75 @@ int prec (char c){
 * when any of its parametrs is NULL
 **/
 Status infix2postfix (char *suf, const char *in){
-  Status ret = TRUE;
   Stack *s = NULL;
   char *ele = NULL;
+  char cpy[1000];
   int i = 0, j = 0;
+
+  if(!in) return ERROR;
+
+  if(!suf) return ERROR;
 
   s=stack_init(char_free, char_copy, char_print);
   if(!s) return ERROR;
 
-  while(in[i] != '\0'){
-    if(isOperator(in[i]) == TRUE){
-      while((stack_isEmpty(s) == FALSE) && (prec(stack_top(s)) >= prec(in[i]))){
+  strcpy(cpy, in);
+
+  while(cpy[i] != '\0'){
+    if(isOperator(cpy[i]) == TRUE){
+      while((stack_isEmpty(s) == FALSE) && (prec((char*)stack_top(s)) >= prec(cpy+i))){
         ele = (char *)stack_pop(s);
-        suf[j] = ele;
+        suf[j] = *ele;
+        free(ele);
         j++;
       }
-      stack_push(s, in[i]);
+      stack_push(s, &cpy[i]);
     }
 
-    else if(in[i] == '('){
-      stack_push(s, in[i]);
+    else if(cpy[i] == '('){
+      stack_push(s, &cpy[i]);
     }
 
-    else if(in[i] == ')'){
-      while((stack_isEmpty(s) == FALSE) && (stack_top(s) != '(')){
+    else if(cpy[i] == ')'){
+      while((stack_isEmpty(s) == FALSE) && (*(char*)stack_top(s)!='(')){
         ele = (char *)stack_pop(s);
-        suf[j] = ele;
+        suf[j] = *ele;
+        free(ele);
         j++;
       }
-      stack_pop(s);
+      ele=(char*)stack_pop(s);
+      free(ele);
     }
 
     else{
-      suf[j] = in[i];
+      suf[j] = cpy[i];
       j++;
     }
     i++;
   }
-
   while(stack_isEmpty(s) == FALSE){
     ele = (char *)stack_pop(s);
-    suf[j] = ele;
+    suf[j] = *ele;
+    free(ele);
     j++;
   }
-
   stack_free(s);
-
   return TRUE;
 }
 
 int main(int argc, char* argv[]){
-  int i;
-  char *suf;
-  char *in;
+  char suf[1000]="suffix";
+  char in[1000]="infix";
 
+  if(argc!=2){
+    fprintf(stdout, "ERROR EN ARGUMENTOS");
+    return -1;
+  }
   strcpy(in, argv[1]);
+  fprintf(stdout, "Input: %s\n", argv[1]);
 
-  infix2postfix(in, suf);
+  infix2postfix(suf, argv[1]);
+  fprintf(stdout, "Output: %s\n", suf);
 
   return 0;
 }
