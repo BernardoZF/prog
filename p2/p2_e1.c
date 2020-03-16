a3c013430d6daaf3c6c90b6e966746e2579c370e
alvarxp <47323193+alvarxp@users.noreply.github.com>
2020-03-16 13:17:44 +0100

    Comentado sin revisar

--- p2/p2_e1.c
+++ p2/p2_e1.c
@@ -32,7 +32,8 @@ int main(){
 char * reverseWords (char *strout, const char *strin){
   Stack *s = NULL;
   int i, j=0;
-
+  
+  /*Inicializamos la pila y su correspondiente control de errores*/
   s = stack_init(char_free, char_copy, char_print);
   if(!s) return ERROR;
   
@@ -40,6 +41,8 @@ char * reverseWords (char *strout, const char *strin){
   printf("%d", i);
   i = 0;
 
+  /*Mediante este bucle damos la vuelta a cada palabra individualmente, cambiando
+  unicamente el orden de las letras y no el de las palabras en las frases*/
   for(i=0; strin[i] != '\0'; i++){
     if(strin[i] == ' '){
       while(stack_isEmpty(s) == FALSE){
@@ -49,14 +52,16 @@ char * reverseWords (char *strout, const char *strin){
       strout[j] = ' ';
       j++;
     }
-    stack_push(s, &strin[i]);
+    stack_push(s, &strin[i]);//Extraemos letra a letra de nuestra cadena
   }
-
+  
+  /*Mientras que la pila este llena, metemos las letras en nuestra cadena de salida*/
   while(stack_isEmpty(s) == FALSE){
     strncpy(&strout[j], (char*)stack_pop(s), 1);
     j++; 
   }
-
+  
+  /*Liberamos la pila*/
   stack_free(s);
 
   return strout;
